1eb9e3c0019c7a2837c7398dafeed7fcac125c50
Daimi Mussey <daimimussey@Daimis-MacBook-Pro.local>
2020-03-29 22:33:22 -0600

    Adding comments to DateListener interface

--- src/main/java/memoranda/date/DateListener.java
+++ src/main/java/memoranda/date/DateListener.java
@@ -3,6 +3,7 @@ package main.java.memoranda.date;
 /*$Id: DateListener.java,v 1.2 2004/01/30 12:17:41 alexeya Exp $*/
 public interface DateListener {
 
+  // Method declared to change the current date to the CalendarDate parameter passed in
   void dateChange(CalendarDate date);
 
 }
