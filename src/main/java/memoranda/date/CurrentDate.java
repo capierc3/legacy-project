714e12a0c7e806c24c3cf33d341c71c0e790e677
Daimi Mussey <daimimussey@Daimis-MacBook-Pro.local>
2020-03-29 22:31:27 -0600

    Adding comments to CurrentDate class

--- src/main/java/memoranda/date/CurrentDate.java
+++ src/main/java/memoranda/date/CurrentDate.java
@@ -19,28 +19,35 @@ public class CurrentDate {
     private static CalendarDate _date = new CalendarDate();
     private static Vector dateListeners = new Vector();
 
+    // Method to return the current CalendarDate selected
     public static CalendarDate get() {
         return _date;
     }
 
+    // Void method that sets the current CalendarDate to the new date parameter passed in
     public static void set(CalendarDate date) {
         if (date.equals(_date)) return;
         _date = date;
         dateChanged(date);
     }
 
+    // Method to reset the CalendarDate to today's date
     public static void reset() {
         set(new CalendarDate());
     }
 
+    // Method to add a DateListener to the Vector dateListeners
     public static void addDateListener(DateListener dl) {
         dateListeners.add(dl);
     }
 
+    // Method to return a Collection from the Vector dateListeners
     public static Collection getChangeListeners() {
         return dateListeners;
     }
 
+    // Void method that gets the DateListener for the CalendarDate object passed in as a parameter, and
+    // calls out to the DateListener interface that the date selected has been changed
     private static void dateChanged(CalendarDate date) {
         for (int i = 0; i < dateListeners.size(); i++)
             ((DateListener)dateListeners.get(i)).dateChange(date);
