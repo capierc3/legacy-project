111a69e6b9f94ad7e96964bf2697e6d7ae31ac1b
Steve Wilson <sbwilso7@asu.edu>
2020-03-31 22:33:12 -0700

    US#5:TASK#24 Remove icontized condition from processWindowEvent

--- src/main/java/memoranda/ui/AppFrame.java
+++ src/main/java/memoranda/ui/AppFrame.java
@@ -667,6 +667,7 @@ public class AppFrame extends JFrame {
     public void doMinimize() {
         exitNotify();
         App.closeWindow();
+
     }
 
     //Help | About action performed
@@ -681,17 +682,14 @@ public class AppFrame extends JFrame {
     }
 
     protected void processWindowEvent(WindowEvent e) {
+
         if (e.getID() == WindowEvent.WINDOW_CLOSING) {
             if (Configuration.get("ON_CLOSE").equals("exit"))
                 doExit();
             else
                 doMinimize();
         }
-        else if ((e.getID() == WindowEvent.WINDOW_ICONIFIED)) {
-            super.processWindowEvent(new WindowEvent(this,
-                    WindowEvent.WINDOW_CLOSING));
-            doMinimize();
-        }
+        
         else
             super.processWindowEvent(e);
     }
