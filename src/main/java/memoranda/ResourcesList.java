d65eb2cbee81a58e32ed9547f6b7e8bc9accad38
Chase <cpierce2113@gmail.com>
2020-04-03 12:02:25 -0400

    comments for the Resource Based classes

--- src/main/java/memoranda/ResourcesList.java
+++ src/main/java/memoranda/ResourcesList.java
@@ -1,38 +1,60 @@
-/**
- * ResourcesList.java
- * Created on 24.03.2003, 18:25:59 Alex
- * Package: net.sf.memoranda
- * 
- * @author Alex V. Alishevskikh, alex@openmechanics.net
- * Copyright (c) 2003 Memoranda Team. http://memoranda.sf.net
- */
 package main.java.memoranda;
 
 import java.util.Vector;
 
 import nu.xom.Document;
-/**
- * 
- */
 /*$Id: ResourcesList.java,v 1.4 2007/03/20 06:21:46 alexeya Exp $*/
+/**
+ * Interface for the list of resources
+ */
 public interface ResourcesList {
-    
+    /**
+     * returns all the resources in the list
+     * @return Vector
+     */
     Vector getAllResources();
     
     //Vector getResourcesForTask(String taskId);
-    
+
+    /**
+     * returns a single resource form the list
+     * @param path String
+     * @return Resource
+     */
     Resource getResource(String path);
-    
+
+    /**
+     * adds a resource to the list.
+     * @param path String
+     * @param isInternetShortcut boolean
+     * @param isProjectFile boolean
+     */
     void addResource(String path, boolean isInternetShortcut, boolean isProjectFile);
-    
+
+    /**
+     * adds a resource to a list with only the path
+     * @param path String
+     */
     void addResource(String path);
     
     //void addResource(String path, String taskId);
-    
+
+    /**
+     * removes a resource with matching path
+     * @param path String
+     */
     void removeResource(String path);
-        
+
+    /**
+     * getter for the amount of resources
+     * @return int
+     */
     int getAllResourcesCount();
-    
+
+    /**
+     * getter for the main XML Document
+     * @return Document
+     */
     Document getXMLContent();
 
 }
