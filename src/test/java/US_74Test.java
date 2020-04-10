5d39125bb469c3890ea2841337ce8431e79a22dd
Justin Oliver <justinoliver827@gmail.com>
2020-04-09 20:00:33 -0700

    US74 added a test file for this US

---
+++ src/test/java/US_74Test.java
@@ -0,0 +1,31 @@
+/* 
+ * @author Justin 
+ * 
+ * This is a class to help with testing
+ * It is designed to be used as a template for either whitebox or blackbox testing
+ * To use, create a copy of this file and name accordingly.
+ * Modify/add any tests you need
+ * Please do not modify this sample test file, it is to be used as a template
+ * All possible imports are currently included at time this sample was created, however 
+ * as this project develops, new directories may be created and this list will need to be updated.
+ * 
+ * To use junit in Gradle, run "gradle build" and "gradle test"
+ * Gradle Junit test reports should populate in Kaesekuchen/build/reports/tests/test...
+ * 
+ * To use jacoco for code coverage report, run "gradle jacocoTestReport"
+ * Gradle jacoco test report should populate in Kaesekuchen/build/reports/jacoco/test/html...
+ * 
+ * 
+ */
+
+/*
+ * The task was the creation of the sampleTest.java file, the test folder, and the addition of Junit and Jacoco to build.gradle. 
+ * I don't know how to test the code coverage from this test specifically, but if you run a code coverage test on sampleTest.Java, you will see that code is covered 100% and that 
+ * the class functions as intended. You can also run a sample a test via a CLI and see the correct .html outputs from Junit and jacoco. 
+ */
+
+
+
+
+
+
