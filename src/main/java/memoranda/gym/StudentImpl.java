a450d37a3052a840a917b7d47068eca4949e1836
Daimi Mussey <daimimussey@Daimis-MacBook-Pro.local>
2020-04-10 22:54:12 -0600

    75 - adding comments

--- src/main/java/memoranda/gym/StudentImpl.java
+++ src/main/java/memoranda/gym/StudentImpl.java
@@ -1,12 +1,23 @@
 package memoranda.gym;
 
+/**
+ * Interface for the methods used for the Student class
+ * @author Daimi Mussey
+ */
 public class StudentImpl implements Student {
     private ClassList list;
 
+    /**
+     * Getter for the collection of classes
+     * @return Collection
+     */
     public ClassList getClasses() {
         return list;
     }
 
+    /**
+     * Adds a class to the list of classes for the student
+     */
     public void addClass(GymClass gymClass) {
         list.addClass(gymClass);
     }
