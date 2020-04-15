75ae3012585a6a4f6cf5cfe6a7acda6c5249e246
Steve Wilson <sbwilso7@asu.edu>
2020-04-14 21:17:02 -0700

    Renamed AppLogin class to AppUsers and implemented all methods from UserList interface.

---
+++ src/main/java/memoranda/gym/AppUsers.java
@@ -0,0 +1,101 @@
+package main.java.memoranda.gym;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * The AppUsers class is a data model that houses information on all application users.
+ */
+
+public class AppUsers implements UserList {
+
+    private final String APP_USER_FILE_PATH = "/appusers.dat";
+    private HashMap<String, User> appUsers;
+
+    /**
+     * Class constructor.  Initiates appUser collection
+     * */
+    public AppUsers() {
+
+        appUsers = new HashMap<>();
+
+    }
+
+    /**
+     * Simple getter method for User
+     *
+     * @param id String value of user ID
+     * @return User object from collection.  Returns null if id does not exist in collection.
+     */
+    public User getUser(String id) {
+
+        if(appUsers.containsKey(id)) {
+            return appUsers.get(id);
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Simple setter method for adding User to collection
+     *
+     * @param user User object to be added to collection
+     * @return void
+     */
+    public void addUser(User user) {
+
+        String id = user.getID();
+
+        if(!appUsers.containsKey(id)){
+            appUsers.put(id, user);
+        }
+
+    }
+
+    /**
+     * Removed user from collection
+     *
+     * @param id User object to be removed from collection
+     * @return void
+     */
+    public void removeUser(String id) {
+
+        if(appUsers.containsKey(id)) {
+
+            appUsers.remove(id);
+
+        }
+
+    }
+
+    /**
+     * Get the size of the collection
+     *
+     * @return int size of collection
+     */
+    public int getSize() {
+
+        return appUsers.size();
+
+    }
+
+    /**
+     * Return entire collection of users
+     *
+     * @return LinkedList of all users in collection.  Returns null if collection is empty.
+     */
+    public Collection<User> getAllUsers() {
+
+        if(appUsers.size() > 0) {
+            LinkedList<User> list = new LinkedList<User>();
+            appUsers.forEach((k,v) -> list.add(v));
+            return list;
+        }
+
+        return null;
+
+    }
+
+}
