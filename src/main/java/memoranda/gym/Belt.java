011daa05b28b2ffc3167fd8f35b8169603c40e56
Chase <cpierce2113@gmail.com>
2020-04-16 15:56:59 -0400

    Task_98: commented the Belt enum class

--- src/main/java/memoranda/gym/Belt.java
+++ src/main/java/memoranda/gym/Belt.java
@@ -2,7 +2,17 @@ package main.java.memoranda.gym;
 
 import java.util.HashMap;
 
-
+/**
+ * Enum Class for the Belts, Each are assigned a value depending on their ordering.
+ * White belt ->0 is lower than Blue ->4.
+ *
+ * You are able be able to get the value of the enum with the getValue() method.
+ *
+ * You are also able to retrieve the enum with inputting its value with getBelt(int value) method.
+ * This allows you to save the value of the enum in the element and quickly turn it back into the enum.
+ *
+ * @author Chase
+ */
 public enum Belt {
     WHITE(0),
     YELLOW(1),
@@ -22,7 +32,11 @@ public enum Belt {
     private int _value;
     private static HashMap belts = new HashMap();
 
-    private Belt(int value){
+    /**
+     * Constructor that adds the value to the enum.
+     * @param value int
+     */
+    Belt(int value){
         _value = value;
     }
 
@@ -32,10 +46,19 @@ public enum Belt {
         }
     }
 
+    /**
+     * returns the enum with the associated int value.
+     * @param value int
+     * @return Belt
+     */
     public static Belt getBelt(int value){
         return (Belt) belts.get(value);
     }
 
+    /**
+     * returns the value associated with the enum.
+     * @return int
+     */
     public int getValue(){
         return _value;
     }
