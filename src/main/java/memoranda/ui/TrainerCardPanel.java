a859e0f955bd1e2f1894d94d9c3d0999906937b1
Justin Oliver <justinoliver827@gmail.com>
2020-04-18 14:12:05 -0700

    107-Enabled actions on the buttons to prove the function. Still need to get them to do they job they are intended to do. But they exist and work currently

--- src/main/java/memoranda/ui/TrainerCardPanel.java
+++ src/main/java/memoranda/ui/TrainerCardPanel.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
@@ -64,7 +65,7 @@ public class TrainerCardPanel extends JPanel {
 
     void jbInit(String name, String belt, String funFact) throws Exception {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        TitledBorder title = BorderFactory.createTitledBorder("KAESEKUCHEN TRAINERS");
+
         
         JLabel trainerName = new JLabel("Trainer Name: " +name+ "\n");
         JLabel trainerBelt = new JLabel("Belt Level: " + belt + "\n");
@@ -97,177 +98,30 @@ public class TrainerCardPanel extends JPanel {
         this.add(trainerInfo);
         this.add(editUser);
         this.setBorder(BorderFactory.createEtchedBorder(Color.blue, Color.yellow));
-        //this.setPreferredSize(new Dimension(250, 250));
+        
+        editUser.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          {
+              editUser();
+          }
+        });
+
     }
     
     
 
-    void editUser_actionPerformed(ActionEvent e) {
-        /*
-         * EventDialog dlg = new EventDialog(App.getFrame(), Local.getString("Event"));
-         * //main.java.memoranda.Event ev = // (main.java.memoranda.Event)
-         * eventsTable.getModel().getValueAt( // eventsTable.getSelectedRow(), //
-         * EventsTable.EVENT);
-         * 
-         * dlg.timeSpin.getModel().setValue(ev.getTime()); if (new
-         * CalendarDate(ev.getTime()).equals(CalendarDate.today()))
-         * ((SpinnerDateModel)dlg.timeSpin.getModel()).setStart(new Date()); else
-         * ((SpinnerDateModel)dlg.timeSpin.getModel()).setStart(CalendarDate.today().
-         * getDate());
-         * ((SpinnerDateModel)dlg.timeSpin.getModel()).setEnd(CalendarDate.tomorrow().
-         * getDate()); dlg.textField.setText(ev.getText()); int rep = ev.getRepeat(); if
-         * (rep > 0) { dlg.startDate.getModel().setValue(ev.getStartDate().getDate());
-         * if (rep == EventsManager.REPEAT_DAILY) { dlg.dailyRepeatRB.setSelected(true);
-         * dlg.dailyRepeatRB_actionPerformed(null);
-         * dlg.daySpin.setValue(ev.getPeriod()); } else if (rep ==
-         * EventsManager.REPEAT_WEEKLY) { dlg.weeklyRepeatRB.setSelected(true);
-         * dlg.weeklyRepeatRB_actionPerformed(null); int d = ev.getPeriod() - 1;
-         * if(Configuration.get("FIRST_DAY_OF_WEEK").equals("mon")) { d--; if(d<0) d=6;
-         * } dlg.weekdaysCB.setSelectedIndex(d); } else if (rep ==
-         * EventsManager.REPEAT_MONTHLY) { dlg.monthlyRepeatRB.setSelected(true);
-         * dlg.monthlyRepeatRB_actionPerformed(null);
-         * dlg.dayOfMonthSpin.setValue(ev.getPeriod()); } else if (rep ==
-         * EventsManager.REPEAT_YEARLY) { dlg.yearlyRepeatRB.setSelected(true);
-         * dlg.yearlyRepeatRB_actionPerformed(null); dlg.dayOfMonthSpin.setValue(new
-         * Integer(ev.getPeriod())); } if (ev.getEndDate() != null) {
-         * dlg.endDate.getModel().setValue(ev.getEndDate().getDate());
-         * dlg.enableEndDateCB.setSelected(true);
-         * dlg.enableEndDateCB_actionPerformed(null); } if(ev.getWorkingDays()) {
-         * dlg.workingDaysOnlyCB.setSelected(true); }
-         * 
-         * }
-         * 
-         * Dimension frmSize = App.getFrame().getSize(); Point loc =
-         * App.getFrame().getLocation(); dlg.setLocation((frmSize.width -
-         * dlg.getSize().width) / 2 + loc.x, (frmSize.height - dlg.getSize().height) / 2
-         * + loc.y); dlg.setVisible(true); if (dlg.CANCELLED) return;
-         * EventsManager.removeEvent(ev);
-         * 
-         * Calendar calendar = new GregorianCalendar(Local.getCurrentLocale()); //Fix
-         * deprecated methods to get hours //by (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM
-         * calendar.setTime(((Date)dlg.timeSpin.getModel().getValue()));//Fix deprecated
-         * methods to get hours //by (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM int hh =
-         * calendar.get(Calendar.HOUR_OF_DAY);//Fix deprecated methods to get hours //by
-         * (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM int mm =
-         * calendar.get(Calendar.MINUTE);//Fix deprecated methods to get hours //by
-         * (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM
-         * 
-         * //int hh = ((Date) dlg.timeSpin.getModel().getValue()).getHours(); //int mm =
-         * ((Date) dlg.timeSpin.getModel().getValue()).getMinutes(); String text =
-         * dlg.textField.getText(); if (dlg.noRepeatRB.isSelected())
-         * EventsManager.createEvent(CurrentDate.get(), hh, mm, text); else {
-         * updateEvents(dlg,hh,mm,text); } saveEvents();
-         */
+    /*
+     * Method to add edit this users card
+     */
+    
+    void editUser() {
+        //TrainerCardPanel testCard7 = new TrainerCardPanel("John Bosworth", "White", "Innovation is a risk.");
+        //this.add(testCard7);
+        JOptionPane.showMessageDialog(null, "This worked");
+        revalidate();
     }
 
-    /*
-     * void newEventB_actionPerformed(ActionEvent e) { Calendar cdate =
-     * CurrentDate.get().getCalendar(); // round down to hour
-     * cdate.set(Calendar.MINUTE, 0); Util.debug("Default time is " + cdate);
-     * 
-     * newEventB_actionPerformed(e, null, cdate.getTime(), cdate.getTime()); }
-     * 
-     * void newEventB_actionPerformed(ActionEvent e, String tasktext, Date
-     * startDate, Date endDate) { EventDialog dlg = new EventDialog(App.getFrame(),
-     * Local.getString("New Class")); Dimension frmSize = App.getFrame().getSize();
-     * Point loc = App.getFrame().getLocation(); if (tasktext != null) {
-     * dlg.textField.setText(tasktext); }
-     * dlg.startDate.getModel().setValue(startDate);
-     * dlg.endDate.getModel().setValue(endDate);
-     * dlg.timeSpin.getModel().setValue(startDate);
-     * 
-     * dlg.setLocation((frmSize.width - dlg.getSize().width) / 2 + loc.x,
-     * (frmSize.height - dlg.getSize().height) / 2 + loc.y);
-     * dlg.setEventDate(startDate); dlg.setVisible(true); if (dlg.CANCELLED) return;
-     * Calendar calendar = new GregorianCalendar(Local.getCurrentLocale()); // Fix
-     * deprecated methods to get hours // by (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM
-     * calendar.setTime(((Date) dlg.timeSpin.getModel().getValue()));// Fix
-     * deprecated methods to get hours // by (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM
-     * int hh = calendar.get(Calendar.HOUR_OF_DAY);// Fix deprecated methods to get
-     * hours // by (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM int mm =
-     * calendar.get(Calendar.MINUTE);// Fix deprecated methods to get hours // by
-     * (jcscoobyrs) 14-Nov-2003 at 10:24:38 AM
-     * 
-     * // int hh = ((Date) dlg.timeSpin.getModel().getValue()).getHours(); // int mm
-     * = ((Date) dlg.timeSpin.getModel().getValue()).getMinutes(); String text =
-     * dlg.textField.getText();
-     * 
-     * CalendarDate eventCalendarDate = new CalendarDate(dlg.getEventDate());
-     * 
-     * if (dlg.noRepeatRB.isSelected()) EventsManager.createEvent(eventCalendarDate,
-     * hh, mm, text); else { updateEvents(dlg, hh, mm, text); } saveEvents(); }
-     * 
-     * private void saveEvents() { CurrentStorage.get().storeEventsManager();
-     * eventsTable.refresh(); EventsScheduler.init();
-     * parentPanel.calendar.jnCalendar.updateUI(); parentPanel.updateIndicators(); }
-     * 
-     * private void updateEvents(EventDialog dlg, int hh, int mm, String text) { int
-     * rtype; int period; CalendarDate sd = new CalendarDate((Date)
-     * dlg.startDate.getModel().getValue()); CalendarDate ed = null; if
-     * (dlg.enableEndDateCB.isSelected()) ed = new CalendarDate((Date)
-     * dlg.endDate.getModel().getValue()); if (dlg.dailyRepeatRB.isSelected()) {
-     * rtype = EventsManager.REPEAT_DAILY; period = (Integer)
-     * dlg.daySpin.getModel().getValue(); } else if
-     * (dlg.weeklyRepeatRB.isSelected()) { rtype = EventsManager.REPEAT_WEEKLY;
-     * period = dlg.weekdaysCB.getSelectedIndex() + 1; if
-     * (Configuration.get("FIRST_DAY_OF_WEEK").equals("mon")) { if (period == 7)
-     * period = 1; else period++; } } else if (dlg.yearlyRepeatRB.isSelected()) {
-     * rtype = EventsManager.REPEAT_YEARLY; period =
-     * sd.getCalendar().get(Calendar.DAY_OF_YEAR); if ((sd.getYear() % 4) == 0 &&
-     * sd.getCalendar().get(Calendar.DAY_OF_YEAR) > 60) period--; } else { rtype =
-     * EventsManager.REPEAT_MONTHLY; period = (Integer)
-     * dlg.dayOfMonthSpin.getModel().getValue(); }
-     * EventsManager.createRepeatableEvent(rtype, sd, ed, period, hh, mm, text,
-     * dlg.workingDaysOnlyCB.isSelected()); }
-     * 
-     * void removeEventB_actionPerformed(ActionEvent e) { String msg;
-     * main.java.memoranda.Event ev;
-     * 
-     * if (eventsTable.getSelectedRows().length > 1) msg = Local.getString("Remove")
-     * + " " + eventsTable.getSelectedRows().length + " " +
-     * Local.getString("events") + "\n" + Local.getString("Are you sure?"); else {
-     * ev = (main.java.memoranda.Event)
-     * eventsTable.getModel().getValueAt(eventsTable.getSelectedRow(),
-     * EventsTable.EVENT); msg = Local.getString("Remove Class") + "\n'" +
-     * ev.getText() + "'\n" + Local.getString("Are you sure?"); }
-     * 
-     * int n = JOptionPane.showConfirmDialog(App.getFrame(), msg,
-     * Local.getString("Remove Class"), JOptionPane.YES_NO_OPTION); if (n !=
-     * JOptionPane.YES_OPTION) return;
-     * 
-     * for (int i = 0; i < eventsTable.getSelectedRows().length; i++) { ev =
-     * (main.java.memoranda.Event)
-     * eventsTable.getModel().getValueAt(eventsTable.getSelectedRows()[i],
-     * EventsTable.EVENT); EventsManager.removeEvent(ev); }
-     * eventsTable.getSelectionModel().clearSelection();
-     * 
-     * CurrentStorage.get().storeEventsManager(); eventsTable.refresh();
-     * EventsScheduler.init(); parentPanel.calendar.jnCalendar.updateUI();
-     * parentPanel.updateIndicators(); saveEvents(); }
-     */
 
-    class PopupListener extends MouseAdapter {
 
-        public void mouseClicked(MouseEvent e) {
-            if ((e.getClickCount() == 2))
-                editUser_actionPerformed(null);
-        }
-
-        /*
-         * public void mousePressed(MouseEvent e) { maybeShowPopup(e); }
-         * 
-         * public void mouseReleased(MouseEvent e) { maybeShowPopup(e); }
-         * 
-         * private void maybeShowPopup(MouseEvent e) { if (e.isPopupTrigger()) {
-         * eventPPMenu.show(e.getComponent(), e.getX(), e.getY()); } }
-         */
-
-    }
-    /*
-     * void ppEditEvent_actionPerformed(ActionEvent e) {
-     * editEventB_actionPerformed(e); } void
-     * ppRemoveEvent_actionPerformed(ActionEvent e) {
-     * removeEventB_actionPerformed(e); } void
-     * ppNewEvent_actionPerformed(ActionEvent e) { newEventB_actionPerformed(e); }
-     */
 }
--- src/main/java/memoranda/ui/TrainerProfilesPanel.java
+++ src/main/java/memoranda/ui/TrainerProfilesPanel.java
@@ -13,6 +13,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.KeyListener;
@@ -22,6 +23,7 @@ import java.io.File;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -55,6 +57,9 @@ public class TrainerProfilesPanel extends JPanel {
     TrainerCardPanel testCard6 = new TrainerCardPanel("John Bosworth", "White", "Innovation is a risk.");
     JButton newUser = new JButton("New Trainer");
 
+    
+    
+
     public TrainerProfilesPanel() {
         try {
             jbInit();
@@ -75,209 +80,40 @@ public class TrainerProfilesPanel extends JPanel {
         this.add(testCard6); 
         this.add(newUser);
         this.setBorder(title);
+        
+        newUser.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          {
+              addUser();
+          }
+        });
+        
         /*
          * PopupListener ppListener = new PopupListener();
-         * scrollPane.addMouseListener(ppListener);
-         * //resourcesTable.addMouseListener(ppListener);
-         * 
-         * 
-         * resourcesTable.getSelectionModel().addListSelectionListener(new
-         * ListSelectionListener() { public void valueChanged(ListSelectionEvent e) {
-         * boolean enbl = (resourcesTable.getRowCount() > 0) &&
-         * (resourcesTable.getSelectedRow() > -1);
-         * 
-         * removeResB.setEnabled(enbl); ppRemoveRes.setEnabled(enbl);
-         * ppRun.setEnabled(enbl); } });
-         * 
-         * refreshB.setBorderPainted(false); refreshB.addActionListener(new
-         * java.awt.event.ActionListener() { public void actionPerformed(ActionEvent e)
-         * { refreshB_actionPerformed(e); } }); refreshB.setFocusable(false);
-         * refreshB.setPreferredSize(new Dimension(24, 24));
-         * refreshB.setRequestFocusEnabled(false);
-         * refreshB.setToolTipText(Local.getString("Refresh"));
-         * refreshB.setMinimumSize(new Dimension(24, 24)); refreshB.setMaximumSize(new
-         * Dimension(24, 24)); refreshB.setEnabled(true); refreshB.setIcon( new
-         * ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource(
-         * "/ui/icons/refreshres.png"))); //resPPMenu.setFont(new
-         * java.awt.Font("Dialog", 1, 10)); ppRun.setFont(new java.awt.Font("Dialog", 1,
-         * 11)); ppRun.setText(Local.getString("Open resource")+"...");
-         * ppRun.addActionListener(new java.awt.event.ActionListener() { public void
-         * actionPerformed(ActionEvent e) { ppRun_actionPerformed(e); } });
-         * ppRun.setEnabled(false);
-         * 
-         * ppRemoveRes.setFont(new java.awt.Font("Dialog", 1, 11));
-         * ppRemoveRes.setText(Local.getString("Remove resource"));
-         * ppRemoveRes.addActionListener(new java.awt.event.ActionListener() { public
-         * void actionPerformed(ActionEvent e) { ppRemoveRes_actionPerformed(e); } });
-         * ppRemoveRes.setIcon(new
-         * ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource(
-         * "/ui/icons/removeresource.png"))); ppRemoveRes.setEnabled(false);
-         * ppNewRes.setFont(new java.awt.Font("Dialog", 1, 11));
-         * ppNewRes.setText(Local.getString("New Trainer")+"..."); // Set this to create
-         * new Trainer ppNewRes.addActionListener(new java.awt.event.ActionListener() {
-         * public void actionPerformed(ActionEvent e) { ppNewRes_actionPerformed(e); }
-         * }); ppNewRes.setIcon(new
-         * ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource(
-         * "/ui/icons/addresource.png")));
-         * 
-         * ppRefresh.setFont(new java.awt.Font("Dialog", 1, 11));
-         * ppRefresh.setText(Local.getString("Refresh"));
-         * ppRefresh.addActionListener(new java.awt.event.ActionListener() { public void
-         * actionPerformed(ActionEvent e) { ppRefresh_actionPerformed(e); } });
+         * newUser.addMouseListener(ppListener);
          */
-        /*
-         * ppRefresh.setIcon(new
-         * ImageIcon(main.java.memoranda.ui.AppFrame.class.getResource(
-         * "/ui/icons/refreshres.png")));
-         * 
-         * toolBar.add(newResB, null); toolBar.add(removeResB, null);
-         * toolBar.addSeparator(); toolBar.add(refreshB, null); this.add(scrollPane,
-         * BorderLayout.CENTER); scrollPane.getViewport().add(resourcesTable, null);
-         * //this.add(toolBar, BorderLayout.NORTH);
-         * 
-         * this.add(testCard, BorderLayout.SOUTH); resPPMenu.add(ppRun);
-         * resPPMenu.addSeparator(); resPPMenu.add(ppNewRes);
-         * resPPMenu.add(ppRemoveRes); resPPMenu.addSeparator();
-         * resPPMenu.add(ppRefresh);
-         * 
-         * // remove resources using the DEL key resourcesTable.addKeyListener(new
-         * KeyListener() { public void keyPressed(KeyEvent e){
-         * if(resourcesTable.getSelectedRows().length>0 &&
-         * e.getKeyCode()==KeyEvent.VK_DELETE) ppRemoveRes_actionPerformed(null); }
-         * public void keyReleased(KeyEvent e){} public void keyTyped(KeyEvent e){} });
-         */
+         
+    }
+    /*
+     * Method to add new user to system and card to this panel
+     */
+    
+    void addUser() {
+        TrainerCardPanel testCard7 = new TrainerCardPanel("John Bosworth", "White", "Innovation is a risk.");
+        this.add(testCard7);
+        JOptionPane.showMessageDialog(null, "This worked");
+        this.remove(newUser);
+        this.add(newUser);
+        revalidate();
     }
 
-    /*
-     * void newResB_actionPerformed(ActionEvent e) { AddResourceDialog dlg = new
-     * AddResourceDialog(App.getFrame(), Local.getString("New resource")); Dimension
-     * frmSize = App.getFrame().getSize(); Point loc = App.getFrame().getLocation();
-     * dlg.setLocation((frmSize.width - dlg.getSize().width) / 2 + loc.x,
-     * (frmSize.height - dlg.getSize().height) / 2 + loc.y); dlg.setVisible(true);
-     * if (dlg.CANCELLED) return; if (dlg.localFileRB.isSelected()) { String fpath =
-     * dlg.pathField.getText(); MimeType mt =
-     * MimeTypesList.getMimeTypeForFile(fpath); if
-     * (mt.getMimeTypeId().equals("__UNKNOWN")) { mt = addResourceType(fpath); if
-     * (mt == null) return; } if (!checkApp(mt)) return; // if file if projectFile,
-     * than copy the file and change url. if (dlg.projectFileCB.isSelected()) {
-     * fpath = copyFileToProjectDir(fpath);
-     * CurrentProject.getResourcesList().addResource(fpath, false, true); } else
-     * CurrentProject.getResourcesList().addResource(fpath);
-     * 
-     * resourcesTable.tableChanged(); } else { if (!Util.checkBrowser()) return;
-     * CurrentProject.getResourcesList().addResource(dlg.urlField.getText(), true,
-     * false); resourcesTable.tableChanged(); } }
-     * 
-     * void removeResB_actionPerformed(ActionEvent e) { int[] toRemove =
-     * resourcesTable.getSelectedRows(); String msg = ""; if (toRemove.length == 1)
-     * msg = Local.getString("Remove the shortcut to resource") + "\n'" +
-     * resourcesTable.getModel().getValueAt(toRemove[0], 0) + "'";
-     * 
-     * else msg = Local.getString("Remove") + " " + toRemove.length + " " +
-     * Local.getString("shortcuts"); msg += "\n" + Local.getString("Are you sure?");
-     * int n = JOptionPane.showConfirmDialog( App.getFrame(), msg,
-     * Local.getString("Remove resource"), JOptionPane.YES_NO_OPTION); if (n !=
-     * JOptionPane.YES_OPTION) return; for (int i = 0; i < toRemove.length; i++) {
-     * CurrentProject.getResourcesList().removeResource( ((Resource)
-     * resourcesTable.getModel().getValueAt(toRemove[i],
-     * ResourcesTable._RESOURCE)).getPath()); } resourcesTable.tableChanged(); }
-     */
-
-    /*
-     * MimeType addResourceType(String fpath) { ResourceTypeDialog dlg = new
-     * ResourceTypeDialog(App.getFrame(), Local.getString("Resource type"));
-     * Dimension dlgSize = new Dimension(420, 300); dlg.setSize(dlgSize); Dimension
-     * frmSize = App.getFrame().getSize(); Point loc = App.getFrame().getLocation();
-     * dlg.setLocation((frmSize.width - dlgSize.width) / 2 + loc.x, (frmSize.height
-     * - dlgSize.height) / 2 + loc.y); dlg.ext = MimeTypesList.getExtension(fpath);
-     * dlg.setVisible(true); if (dlg.CANCELLED) return null; int ix =
-     * dlg.getTypesList().getSelectedIndex(); MimeType mt = (MimeType)
-     * MimeTypesList.getAllMimeTypes().toArray()[ix];
-     * mt.addExtension(MimeTypesList.getExtension(fpath));
-     * CurrentStorage.get().storeMimeTypesList(); return mt; }
-     * 
-     * boolean checkApp(MimeType mt) { String appId = mt.getAppId(); AppList appList
-     * = MimeTypesList.getAppList(); File d; if (appId == null) { appId =
-     * Util.generateId(); d = new File("/"); } else { File exe = new
-     * File(appList.getFindPath(appId) + "/" + appList.getExec(appId)); if
-     * (exe.isFile()) return true; d = new File(exe.getParent()); while
-     * (!d.exists()) d = new File(d.getParent()); } SetAppDialog dlg = new
-     * SetAppDialog(App.getFrame(), Local.getString(
-     * Local.getString("Select the application to open files of type") + " '" +
-     * mt.getLabel() + "'")); Dimension dlgSize = new Dimension(420, 300);
-     * dlg.setSize(dlgSize); Dimension frmSize = App.getFrame().getSize(); Point loc
-     * = App.getFrame().getLocation(); dlg.setLocation((frmSize.width -
-     * dlgSize.width) / 2 + loc.x, (frmSize.height - dlgSize.height) / 2 + loc.y);
-     * dlg.setDirectory(d); dlg.appPanel.argumentsField.setText("$1");
-     * dlg.setVisible(true); if (dlg.CANCELLED) return false; File f = new
-     * File(dlg.appPanel.applicationField.getText());
-     * 
-     * appList.addOrReplaceApp(appId, f.getParent().replace('\\', '/'),
-     * f.getName().replace('\\', '/'), dlg.appPanel.argumentsField.getText());
-     * mt.setApp(appId); CurrentStorage.get().storeMimeTypesList(); return true; }
-     */
 
 
 
 
 
-    /*
-     * class PopupListener extends MouseAdapter {
-     * 
-     * public void mouseClicked(MouseEvent e) { if ((e.getClickCount() == 2) &&
-     * (resourcesTable.getSelectedRow() > -1)) { String path = (String)
-     * resourcesTable.getValueAt(resourcesTable.getSelectedRow(), 3); if
-     * (path.length() >0) runApp(path); else runBrowser((String)
-     * resourcesTable.getValueAt(resourcesTable.getSelectedRow(), 0)); }
-     * //editTaskB_actionPerformed(null); }
-     * 
-     * public void mousePressed(MouseEvent e) { maybeShowPopup(e); }
-     * 
-     * public void mouseReleased(MouseEvent e) { maybeShowPopup(e); }
-     * 
-     * private void maybeShowPopup(MouseEvent e) { if (e.isPopupTrigger()) {
-     * resPPMenu.show(e.getComponent(), e.getX(), e.getY()); } }
-     * 
-     * } void refreshB_actionPerformed(ActionEvent e) {
-     * resourcesTable.tableChanged(); }
-     * 
-     * void ppRun_actionPerformed(ActionEvent e) { String path = (String)
-     * resourcesTable.getValueAt(resourcesTable.getSelectedRow(), 3); if
-     * (path.length() >0) runApp(path); else runBrowser((String)
-     * resourcesTable.getValueAt(resourcesTable.getSelectedRow(), 0)); } void
-     * ppRemoveRes_actionPerformed(ActionEvent e) { removeResB_actionPerformed(e); }
-     * void ppNewRes_actionPerformed(ActionEvent e) { newResB_actionPerformed(e); }
-     * 
-     * void ppRefresh_actionPerformed(ActionEvent e) {
-     * resourcesTable.tableChanged(); }
-     */
-    /**
-     * Copy a file to the directory of the current project
-     * 
-     * @param srcStr  The path of the source file.
-     * @param destStr The destination path.
-     * @return The new path of the file.
-     */
-    /*
-     * String copyFileToProjectDir(String srcStr) {
-     * 
-     * String JN_DOCPATH = Util.getEnvDir();
-     * 
-     * String baseName; int i = srcStr.lastIndexOf(File.separator); if (i != -1) {
-     * baseName = srcStr.substring(i + 1); } else baseName = srcStr;
-     * 
-     * String destStr = JN_DOCPATH + CurrentProject.get().getID() + File.separator +
-     * "_projectFiles" + File.separator + baseName;
-     * 
-     * File f = new File(JN_DOCPATH + CurrentProject.get().getID() + File.separator
-     * + "_projectFiles"); if (!f.exists()) { f.mkdirs(); }
-     * System.out.println("[DEBUG] Copy file from: " + srcStr + " to: " + destStr);
-     * 
-     * try { FileInputStream in = new FileInputStream(srcStr); FileOutputStream out
-     * = new FileOutputStream(destStr); byte[] buf = new byte[4096]; int len; while
-     * ((len = in.read(buf)) > 0) { out.write(buf, 0, len); } out.close();
-     * in.close(); } catch (IOException e) { System.err.println(e.toString()); }
-     * 
-     * return destStr; }
-     */
+
+
+     
 }
