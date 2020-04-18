c49ec8a96d3d2dba80236323bd6d063f1326b0fd
Daimi Mussey <daimimussey@Daimis-MacBook-Pro.local>
2020-04-18 12:12:21 -0600

    120 - adding OwnerImpl

--- src/main/java/memoranda/gym/Owner.java
+++ src/main/java/memoranda/gym/Owner.java
@@ -1,10 +1,12 @@
 package main.java.memoranda.gym;
 
+import nu.xom.Element;
+import main.java.memoranda.gym.Trainer;
+
 /**
  * Interface for the Owner class
  */
 public interface Owner extends Trainer{
 
-    
 
 }
