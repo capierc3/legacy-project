5e79ee2b92147331755d4e6a29b1819f99ef5adc
Steve Wilson <sbwilso7@asu.edu>
2020-04-28 22:04:22 -0700

    Add shell for ObjectSerializer.java

---
+++ src/main/java/memoranda/gym/ObjectSerializer.java
@@ -0,0 +1,33 @@
+package main.java.memoranda.gym;
+
+import java.io.Serializable;
+
+public class ObjectSerializer implements Serializable {
+
+    /**
+     * Default Constructor.
+     */
+    public ObjectSerializer () {
+
+    }
+
+    /**
+     * Serialize object to file.
+     * @param obj Object to be serialized
+     * @param filepath File path object is to be saved to
+     */
+    public void serializeObject(Object obj, String filepath) {
+
+    }
+
+    /**
+     * Deserialize object from file.
+     * @param filepath File path object is to be extracted from
+     * @return Object deserialized from file
+     */
+    public Object deserializeObject(String filepath) {
+
+        return null;
+    }
+
+}
