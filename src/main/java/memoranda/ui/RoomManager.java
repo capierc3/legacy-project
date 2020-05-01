d4a52b1ee6075dcaf5f49e60f036b58bf182462b
Chase <cpierce2113@gmail.com>
2020-05-01 14:46:29 -0400

    US_130: fixed checkstyles on uncommented code

--- src/main/java/memoranda/ui/RoomManager.java
+++ src/main/java/memoranda/ui/RoomManager.java
@@ -1,30 +1,38 @@
 package main.java.memoranda.ui;
 
-import main.java.memoranda.date.CalendarDate;
 import main.java.memoranda.gym.*;
 import nu.xom.*;
 
 import java.io.*;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+/**
+ * class that loads and saves room information.
+ */
 public class RoomManager {
 
     private HashMap<Integer,Room> rooms;
     private Element element;
 
+    /**
+     * Main constructor that loads the room info from the xml.
+     */
     RoomManager() {
         element = new Element("Rooms");
         rooms = new HashMap<>();
         try {
             loadRooms();
-        } catch (IOException | ParsingException | URISyntaxException e) {
+        } catch (IOException | ParsingException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * adds a room to the room list.
+     * @param room Room
+     */
     public void addRoom(Room room) {
         if (!rooms.containsKey(room.getRoomNum())) {
             rooms.put(room.getRoomNum(), room);
@@ -38,10 +46,17 @@ public class RoomManager {
         return rooms.get(roomNum);
     }
 
+    /**
+     * returns a collection of all the room objects.
+     * @return
+     */
     public Collection<Room> getAllRooms() {
         return rooms.values();
     }
 
+    /**
+     * saves the rooms to an xml file.
+     */
     public void saveRooms() {
         try {
             ObjectSerializer.serializeElement(element,"server/rooms.xml");
@@ -50,7 +65,12 @@ public class RoomManager {
         }
     }
 
-    public void loadRooms() throws IOException, ParsingException, URISyntaxException {
+    /**
+     * loads the rooms from file.
+     * @throws IOException e
+     * @throws ParsingException e
+     */
+    public void loadRooms() throws IOException, ParsingException {
         Builder parser;
         File roomFile = new File("server/rooms.xml");
         if (roomFile.exists()) {
